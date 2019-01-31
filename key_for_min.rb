eadecdfce447bde5eb8ab2db84f16ad84024323d
Alonso Ortega <alonsojortega@gmail.com>
2019-01-31 02:14:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,13 +3,15 @@
 
 def key_for_min_value(name_hash)
   lowest_key = nil
-  lowest_value = nil
+  lowest_val = nil
+
   name_hash.each do |k, v|
-    if lowest_value == nil || v < lowest_value
-      lowest_value = v
+    if lowest_val == nil || v < lowest_val
+      lowest_val = v
       lowest_key = k
     end
   end
+
   lowest_key
 
 end
