74403f8b00c0818c00f3176d89370489c341b161
Alonso Ortega <alonsojortega@gmail.com>
2019-01-31 02:10:49 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  
+  lowest = nil
+
+  name_hash.each do |k, v|
+    if v < lowest || lowest == nill
+      
+    end
+  end
 
 end
